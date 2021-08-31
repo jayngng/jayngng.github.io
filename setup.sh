eb6687316090df7eeeb7f2ed02843fb7f430cc23
Jay Nguyen <cuongnguyen29112@gmail.com>
2021-09-01 09:38:02 +1000

    Add new site

---
+++ setup.sh
@@ -0,0 +1,12 @@
+cd ~/github_stuff/jayngng.github.io/
+rm -r ~/github_stuff/jayngng.github.io/*
+git add ~/github_stuff/jayngng.github.io/.
+git commit -m "Fixing some URL stuff" ~/github_stuff/jayngng.github.io/.
+SSH_GIT_COMMAND="ssh -i ~/.ssh/id_rsa" git push git@github.com:jayngng/jayngng.github.io.git
+cd ~/github_stuff/typing-jekyll-template/
+yarn build
+cp -r ~/github_stuff/typing-jekyll-template/public/* ~/github_stuff/jayngng.github.io/
+cd ~/github_stuff/jayngng.github.io/
+git add ~/github_stuff/jayngng.github.io/.
+git commit -m "Add new site" ~/github_stuff/jayngng.github.io/.
+SSH_GIT_COMMAND="ssh -i ~/.ssh/id_rsa" git push git@github.com:jayngng/jayngng.github.io.git
